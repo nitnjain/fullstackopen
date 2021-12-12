7eccdd514c7a94e81194821146807c23bfa30924
Nitin Jain <nitinfyi@gmail.com>
2021-12-12 22:41:38 +0530

    made changes to support part5 frontend

--- part4/blog-backend/controllers/blogs.js
+++ part4/blog-backend/controllers/blogs.js
@@ -70,7 +70,7 @@ blogsRouter.put('/:id', async (req, res, next) => {
     likes: body.likes
   }
 
-  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, { new: true })
+  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, { new: true }).populate('user', { username: 1, name: 1 })
   res.json(updatedBlog)
 })
 
